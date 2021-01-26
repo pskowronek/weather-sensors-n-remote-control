31884417405f0da34dba399a93bdbde4f3f808a9
Piotr Skowronek <piotr@skowro.net>
2021-01-27 00:32:56 +0100

    Fixed service

--- rpi/04_enable_service.sh
+++ rpi/04_enable_service.sh
@@ -5,7 +5,7 @@ echo "Going to copy gateway.service to /etc/systemd/system..."
 sudo cp *.service /etc/systemd/system/
 
 echo "Service has just been copied. You can start or enable/disable it anytime by invoking:"
-echo "sudo systemctl [start|enable|disable] gateway.service"
+echo "sudo systemctl [start|enable|disable|status] gateway.service"
 echo
 
 echo "Going to enable gateway.service to start on boot time..."
